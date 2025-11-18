a934699c6b804a8f82dbc3fd95988a7c66ca58b4
Nikolay Nikiforuk <nnp.nikiforuk@gmail.com>
2025-11-18 21:30:56 +0200

    created all logic*

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use sctrict';
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const cards = document.querySelectorAll('.dragons__card');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+});
+
+cards.forEach((card) => {
+  const soundSrc = card.dataset.sound;
+  const audio = new Audio(soundSrc);
+
+  card.addEventListener('mouseenter', () => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+});
